ce02b88775aa939a048cff6996c0d9e56ece4271
halaalfandi <130828532+halaalfandi@users.noreply.github.com>
2023-04-18 19:47:11 +0200

    Chats
    add

---
+++ src/mlti/client/ChatServerSync.java
@@ -0,0 +1,64 @@
+package mlti.client;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ChatServerSync {
+
+    // The server socket.
+    private static ServerSocket serverSocket = null;
+    // The client socket.
+    private static Socket clientSocket = null;
+
+    // This chat server can accept up to maxClientsCount clients' connections.
+    private static final int maxClientsCount = 10;
+    private static final ClientThread[] threads = new ClientThread[maxClientsCount];
+
+    public static void main(String args[]) {
+
+        // The default port number.
+        int portNumber = 2222;
+        if (args.length < 1) {
+            System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
+                    + "Now using port number=" + portNumber);
+        } else {
+            portNumber = Integer.valueOf(args[0]).intValue();
+        }
+
+        /*
+         * Open a server socket on the portNumber (default 2222). Note that we can
+         * not choose a port less than 1023 if we are not privileged users (root).
+         */
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                int i = 0;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        (threads[i] = new ClientThread(clientSocket, threads)).start();
+                        break;
+                    }
+                }
+                if (i == maxClientsCount) {
+                    PrintStream os = new PrintStream(clientSocket.getOutputStream());
+                    os.println("Server too busy. Try later.");
+                    os.close();
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+}
---
+++ src/mlti/client/ClientServer.java
@@ -0,0 +1,23 @@
+package mlti.client;
+
+import javax.swing.JOptionPane;
+
+
+public class ClientServer {
+    public static void main(String [] args){
+
+        Object[] selectioValues = { "Server","Client"};
+        String initialSection = "Server";
+
+        Object selection = JOptionPane.showInputDialog(null, "Login as : ", "MyChatApp", JOptionPane.QUESTION_MESSAGE, null, selectioValues, initialSection);
+        if(selection.equals("Server")){
+            String[] arguments = new String[] {};
+            new ChatServerSync().main(arguments);
+        }else if(selection.equals("Client")){
+            String IPServer = JOptionPane.showInputDialog("Enter the Server ip adress");
+            String[] arguments = new String[] {IPServer};
+            new ClientChat().main(arguments);
+        }
+
+    }
+}
